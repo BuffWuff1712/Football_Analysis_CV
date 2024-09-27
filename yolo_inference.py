e26dc6f7e82b16f0cd6b8a76a7a10f17be90663f
BuffWuff1712 <lowbingheng14@gmail.com>
2024-09-28 00:27:44 +0800

    Train first model to identify players, referees and ball

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+model = YOLO('models/best.pt')
 
-results = model.predict('videos/08fd33_4.mp4', save=True)
+results = model.predict('input_videos/08fd33_4.mp4', save=True)
 
 print(results[0])
 print('==================================================')
